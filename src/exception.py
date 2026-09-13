7e6cee21cea90f745be85f83741ca2a1abaf0082
Sindhu <sindhu.koya2020@gmail.com>
2026-09-13 17:25:01 +0530

    EDA and modeal trianing

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+from src.logger import logging
+
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -22,7 +23,7 @@ if __name__ == "__main__":
     try:
         a= 1/0
     except Exception as e:
-        logging.error("Dividebyzero error")
+        logging.info("Dividebyzero error")
         raise customException(e,sys)
       
     
